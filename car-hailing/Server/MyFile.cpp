6b94bd9a7757ce8bfeced28a546c5eef8ede51ed
G2021lyq <65072383+G2021lyq@users.noreply.github.com>
2023-10-16 22:42:30 +0800

    添加了历史记录类，文件类开了个头
    1

---
+++ car-hailing/Server/MyFile.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "MyFile.h"
+
+void MyFile::SetFilePath(CString f_path)
+{
+	filePath = f_path;
+}
+
+bool MyFile::OpenFile()
+{
+	return file.Open(filePath, CFile::modeCreate | CFile::modeWrite | CFile::typeText | CFile::modeNoTruncate);
+}
+
+bool MyFile::OpenFile(CString f_path)
+{
+	SetFilePath(f_path);
+	return OpenFile();
+}
+
+void MyFile::WriteString(CString textToAppend)
+{
+	CStringW Str(textToAppend);
+	file.WriteString(Str);
+}
+
+void MyFile::CloseFile()
+{
+	file.Close();
+}
+
+void MyFile::My_Try()
+{
+
+	//return TRUE;
+}
